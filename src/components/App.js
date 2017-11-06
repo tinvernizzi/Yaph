7470f0e52fd267a1d06ecc80c7dbec9d24d5feae
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2017-11-06 18:09:28 +0100

    Update
    Pour Marvin

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.PureComponent {
+  render(){
+  return  <div> Bonjour </div>;
+  }
+}
+
+export default App;
